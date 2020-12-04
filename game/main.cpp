ab4481537a9c9f536f459f839424e4aa03d87659
sand116 <sandsand1166@gmail.com>
2020-12-04 17:15:22 +0900

    merge

--- game/main.cpp
+++ game/main.cpp
@@ -1,36 +1,29 @@
-
+#include <iostream>
 #include <bangtal>
-#include<iostream>
-#include <time.h>
+#include <ctime>
+#include <string>
+#include <string.h>
+
 using namespace bangtal;
 using namespace std;
 
-/*
-게임 설명 :
-5초마다 로켓이 날라온다.
 
-맵 초기화 : 2560 x 1680 이므로 80 x 80 크기 단위의  네모로 나누면 map[21][32]
+// 키보드 
+enum{
+		UP = 84,
+		LEFT = 82,
+		RIGHT = 83,
+		DOWN = 85,
+		SPACEBAR = 75
+};
 
-막힌 구간 : 0
-움직일 수 있는 구간 : 1
-용감이 현재 위치 : 2
-시작점 : 3
-끝점 : 4
-열쇠 위치 : 5
-왕관 위치 : 6
-
-
-화면 초기화
-보여지는 화면의 크기가 1280 x 720 이므로 scene[8][16]
-
-
-*/
-
+// 좌표 위치
 typedef struct _pos {
 	int x = 0;
 	int y = 0;
 }Pos;
 
+// Game3 map
 int map[21][32] = {
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -62,6 +55,113 @@ Pos now;
 Pos criteria;
 
 
+// GAME 2 함수
+void makingImage(string filename[5], string number[10], ObjectPtr comboNum[10], int& num_index, int& index, ScenePtr& gamescene) {
+	for (num_index = 0; num_index < 10; num_index++) {
+		number[num_index] = "images2/" + to_string(num_index + 1) + ".png";
+		comboNum[num_index] = Object::create(number[num_index], gamescene, 515, 500, false);
+		comboNum[num_index]->setScale(3.0f);
+	}
+	for (index = 0; index < 5; index++) {
+		filename[index] = "images2/ingredient" + to_string(index + 1) + ".png";
+	}
+}
+
+void ingredientCase(int& num, int& i, int& level, int question[], ObjectPtr q_ingredient[], string filename[], ScenePtr& gamescene, int questX, int questY[], ObjectPtr& q_bread1) {
+	for (i = 0; i < level; i++) {
+		num = rand() % 5;
+		switch (num) {
+		case 0:
+			question[i] = UP;
+			q_ingredient[i] = Object::create(filename[0], gamescene, questX, questY[i]);
+			q_ingredient[i]->setScale(1.5f);
+			break;
+		case 1:
+			question[i] = RIGHT;
+			q_ingredient[i] = Object::create(filename[1], gamescene, questX, questY[i]);
+			q_ingredient[i]->setScale(1.5f);
+			break;
+		case 2:
+			question[i] = LEFT;
+			q_ingredient[i] = Object::create(filename[2], gamescene, questX, questY[i]);
+			q_ingredient[i]->setScale(1.5f);
+			break;
+		case 3:
+			question[i] = DOWN;
+			q_ingredient[i] = Object::create(filename[3], gamescene, questX, questY[i]);
+			q_ingredient[i]->setScale(1.5f);
+			break;
+		case 4:
+			question[i] = SPACEBAR;
+			q_ingredient[i] = Object::create(filename[4], gamescene, questX, questY[i]);
+			q_ingredient[i]->setScale(1.5f);
+			break;
+		}
+	}
+	q_bread1->locate(gamescene, questX, questY[i]);
+	q_bread1->show();
+}
+
+void ingredient2case(int& q_index, int& key_flag, int question[], ObjectPtr ingredient[], string filename[], ScenePtr& gamescene, int& answerX, int answerY[]) {
+	switch (question[q_index]) {
+	case UP:
+		ingredient[q_index] = Object::create(filename[0], gamescene, answerX, answerY[q_index]);
+		ingredient[q_index]->setScale(1.5f);
+		break;
+	case RIGHT:
+		ingredient[q_index] = Object::create(filename[1], gamescene, answerX, answerY[q_index]);
+		ingredient[q_index]->setScale(1.5f);
+		break;
+	case LEFT:
+		ingredient[q_index] = Object::create(filename[2], gamescene, answerX, answerY[q_index]);
+		ingredient[q_index]->setScale(1.5f);
+		break;
+	case DOWN:
+		ingredient[q_index] = Object::create(filename[3], gamescene, answerX, answerY[q_index]);
+		ingredient[q_index]->setScale(1.5f);
+		break;
+	case SPACEBAR:
+		ingredient[q_index] = Object::create(filename[4], gamescene, answerX, answerY[q_index]);
+		ingredient[q_index]->setScale(1.5f);
+	}
+	cout << "correct" << endl;
+	key_flag = 1;
+	q_index++;
+}
+void clearArray(int key_press[], ObjectPtr ingredient[], ObjectPtr q_ingredient[], int& q_index, int& key_index, int& key_flag) {
+	for (int i = 0; i < 100; i++) {
+		key_press[i] = 0;
+	}
+	for (int i = 0; i < 5; i++) {
+		ingredient[i]->hide();
+		ingredient[i] = nullptr;
+	}
+	for (int i = 0; i < 5; i++) {
+		q_ingredient[i]->hide();
+		q_ingredient[i] = nullptr;
+	}
+	q_index = 0;
+	key_index = 0;
+	key_flag = 0;
+}
+void makeTimer(ObjectPtr& bread1, ObjectPtr& clear, ObjectPtr& combo, ObjectPtr comboNum[], int& cnt, int& q_index, int& answerX, int answerY[], TimerPtr& breadTimer, TimerPtr& clearTimer, TimerPtr& comboTimer, ScenePtr& gamescene) {
+	bread1->locate(gamescene, answerX, answerY[q_index]);
+	bread1->show();
+	clear->show();
+	combo->show();
+	comboNum[cnt - 1]->show();
+	cnt++;
+	breadTimer->set(0.2f);
+	breadTimer->start();
+	clearTimer->set(0.4f);
+	clearTimer->start();
+	comboTimer->set(0.4f);
+	comboTimer->start();
+}
+
+
+
+//GAME3함수
 
 void init(ScenePtr scene, ObjectPtr* rockets, ObjectPtr* hearts)
 {
@@ -75,12 +175,12 @@ void init(ScenePtr scene, ObjectPtr* rockets, ObjectPtr* hearts)
 
 	for (int i = 0; i < 20; i++)
 	{
-		rockets[i] = Object::create("images/rocket.png", scene, 1280, 0);
+		rockets[i] = Object::create("images3/rocket.png", scene, 1280, 0);
 	}
 
 	for (int i = 0; i < 3; i++)
 	{
-		hearts[i] = Object::create("images/filledheart.png", scene, 700 + 80 * i, 600);
+		hearts[i] = Object::create("images3/filledheart.png", scene, 700 + 80 * i, 600);
 	}
 	return;
 }
@@ -136,7 +236,7 @@ void rocketInitPos(Pos* rocketPos)
 
 	for (int i = 0; i < 20; i++)
 	{
-		rocketPos[i].x = 16 + rand() % 4; // 화면 밖의 좌표에 설정
+		rocketPos[i].x = 16 + rand() % 8; // 화면 밖의 좌표에 설정
 		rocketPos[i].y = rand() % 21;
 	}
 	return;
@@ -155,7 +255,7 @@ bool moveRocket(ScenePtr scene, ObjectPtr* rockets, Pos* rocketPos, int xspeed, 
 
 	for (int i = 0; i < 20; i++)
 	{
-		x = rocketPos[i].x * 80 - xspeed * 4; //좌표 계속 변경
+		x = rocketPos[i].x * 80 - xspeed * 8; //좌표 계속 변경
 		y = rocketPos[i].y * 80 + (criteria.y * 80);
 
 		rockets[i]->locate(scene, x, y);
@@ -177,38 +277,93 @@ bool moveRocket(ScenePtr scene, ObjectPtr* rockets, Pos* rocketPos, int xspeed, 
 	return finishCount == 20;
 }
 
+//메인
 int main()
 {
+	unsigned int srand(time(NULL));
 	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
 	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
-	srand(time(NULL));
+	
+// GAME 2 변수
+	//-----start scene ----------
+	auto startscene = Scene::create("Start Game", "images2/startscene.png");
+
+	// game scene
+	auto gamescene = Scene::create("Hamburger Game", "images2/backgroundscene.png");
+
+	auto startbtn = Object::create("images2/startbtn.png", startscene, 550, 400);
+	auto badbgm = Sound::create("images2/badbgm.mp3");
+	auto bigmac = Sound::create("images2/bigmac.mp3");
+	badbgm->play(true);
+	showMessage("나에게 10개의 햄버거를 각 햄버거당 5초만에 만들어주면 너에게 날개를 주겠다!!");
+
+	startbtn->setScale(0.25f);
+	startbtn->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		gamescene->enter();
+		bigmac->play(true);
+		badbgm->stop();
+		return true;
+		});
+
+
+	int level = 5;
+	int num = 0;
+	int questX = 200;
+	int answerX = 830;
+	int answerY[10] = { 317,354,391,428,465,502,539,576,613,650 };
+	int questY[10] = { 317,354,391,428,465,502,539,576,613,650 };
+	int i = 0;
+	int question[10] = { 0 };
+
+	auto clear = Object::create("images2/clear.png", gamescene, 500, 320, false);
+	clear->setScale(1.5f);
+	auto combo = Object::create("images2/combo.png", gamescene, 570, 500, false);
+	combo->setScale(1.5f);
+	auto bread2 = Object::create("images2/bread2.png", gamescene, 830, 290);
+	auto q_bread2 = Object::create("images2/bread2.png", gamescene, 200, 290);
+	auto q_bread1 = Object::create("images2/bread1.png", gamescene, questX, questY[0], false);
+	auto bread1 = Object::create("images2/bread1.png", gamescene, answerX, answerY[0], false);
+	auto timer = Timer::create(0.1f);
+	auto basetimer = Timer::create(4.0f);
+	auto combosound = Sound::create("images2/gamesound.mp3");
+	bread1->setScale(3.0f);
+	q_bread1->setScale(3.0f);
+
+	ObjectPtr q_ingredient[5] = { 0 };
+	ObjectPtr ingredient[5] = { 0 };
+	ObjectPtr comboNum[10] = { 0 };
+	string filename[5];
+	string number[10];
+	int index = 0;
+	int num_index = 0;
+	int key_index = 0;
+	int q_index = 0;
+	int key_press[100] = { 0 };
+	int key_flag = 0;
+	int cnt = 1;
+	q_bread2->setScale(3.0f);
+	bread2->setScale(3.0f);
+	basetimer->set(40.f);
+
+
+//GAME 3 변수
 
 	//1280 * 720 화면
-	auto scene = Scene::create("sea", "images/sea.png");
+	auto scene = Scene::create("escape game", "images3/sea.png");
 
 	// garden 크기 : 2560 x 1680  
 
 	// 맵의 맨 왼쪽, 맨 아랫 상단은 초기 scene에서 (480,-960) 좌표에 위치한다.
-	auto garden = Object::create("images/garden2.png", scene, 480, -960);
-	auto character = Object::create("images/yonggam7.png", scene, 480, 165);
+	auto garden = Object::create("images3/garden2.png", scene, 480, -960);
+	auto character = Object::create("images3/yonggam7.png", scene, 480, 165);
 
 	// 콘, 왕관
-	auto crown = Object::create("images/crown.png", scene, 480 + 18 * 80, -960 + 18 * 80); //가든 모서리를 기준으로 (18 * 80 ,18*80 ) 떨어진 곳에 위치  480 + (18*80) , -960 + 18 * 80 
-	auto con = Object::create("images/key.png", scene, 480 + 24 * 80, -960); // 가든 모서리를 기준으로 (24 * 80 , 0) 떨어진 곳에 위치 480 + (24*80), -960 
+	auto crown = Object::create("images3/crown.png", scene, 480 + 18 * 80, -960 + 18 * 80); //가든 모서리를 기준으로 (18 * 80 ,18*80 ) 떨어진 곳에 위치  480 + (18*80) , -960 + 18 * 80 
+	auto con = Object::create("images3/key.png", scene, 480 + 24 * 80, -960); // 가든 모서리를 기준으로 (24 * 80 , 0) 떨어진 곳에 위치 480 + (24*80), -960 
 	int find = 0;
-	/*
-	crown->setOnPickCallback([](ObjectPtr object)->bool {
 
-		object->setImage("Images/문서.png");
-
-		});
-	con-> setOnPickCallback([](ObjectPtr object)->bool {
-
-		object->setImage("Images/문서.png");
-		});
-		*/
-		// 안내 지도
-	auto map = Object::create("images/map.png", scene, 1000, 500);
+	// 안내 지도
+	auto map = Object::create("images3/map.png", scene, 1000, 500);
 
 	// 날라오는 로켓들 
 	ObjectPtr rockets[20];
@@ -226,7 +381,7 @@ int main()
 	rocketInitPos(rocketPos);
 
 	// 폭발
-	auto explode = Object::create("images/explode.png", scene, 480, 160);
+	auto explode = Object::create("images3/explode.png", scene, 480, 160);
 	explode->hide();
 
 
@@ -234,9 +389,99 @@ int main()
 	// 폭발했을 때를 위한 제어
 	auto rocketStopTimer = Timer::create(0.1f);
 
+	// 이동제어 - 키보드 입력
+
+	int pressedkey = 0;
+	int moveX, moveY = 0;
+	auto keyMoveTimer = Timer::create(0.1f);
+
+
+	// 로켓의 움직임을 제어 
+	auto rocketMoveTimer = Timer::create(0.5f);
+
+
+
+
+//GAME2 제어함수 
+
+	makingImage(filename, number, comboNum, num_index, index, gamescene);
+
+	timer->setOnTimerCallback([&](TimerPtr t)->bool {
+		ingredientCase(num, i, level, question, q_ingredient, filename, gamescene, questX, questY, q_bread1);
+		return true;
+		});
+	timer->start();
+	showTimer(basetimer);
+
+	auto clearTimer = Timer::create(0.3f);
+	clearTimer->setOnTimerCallback([&](TimerPtr clearT)->bool {
+		clear->hide();
+		return true;
+		});
+	auto comboTimer = Timer::create(0.3f);
+	comboTimer->setOnTimerCallback([&](TimerPtr comboT)->bool {
+		combo->hide();
+		for (auto& e : comboNum) {
+			e->hide();
+		}
+		combosound->stop();
+		return true;
+		});
+	int flag = 0;
+	auto breadTimer = Timer::create(0.3f);
+	breadTimer->setOnTimerCallback([&](TimerPtr breadT)->bool {
+		bread1->hide();
+		if (1) {
+			breadT->set(0.3f);
+		}
+		flag = 1;
+		return true;
+		});
+
+	basetimer->setOnTimerCallback([&](TimerPtr baseT)->bool {
+		showMessage("YOU LOSE");
+		//endGame();
+		return true;
+		});
+
+	gamescene->setOnKeyboardCallback([&](ScenePtr gamescene, int key, bool pressed)->bool {
+		basetimer->start();
+		key_press[key_index] = key;
+		timer->stop();
+		if (key_index % 2 == 0) {
+			if (question[q_index] == key_press[key_index]) {
+				ingredient2case(q_index, key_flag, question, ingredient, filename, gamescene, answerX, answerY);
+			}
+			else if (question[q_index] != key_press[key_index] && question[q_index] != 0) {
+				showMessage("YOU LOSE");
+				//endGame();
+			}
+			else if (question[q_index] == 0 && key_flag == 1) {
+				basetimer->set(40.0f);
+				combosound->play(true);
+				basetimer->stop();
+				makeTimer(bread1, clear, combo, comboNum, cnt, q_index, answerX, answerY, breadTimer, clearTimer, comboTimer, gamescene);
+				clearArray(key_press, ingredient, q_ingredient, q_index, key_index, key_flag);
+				if (cnt > 1) {
+					basetimer->stop();
+					bigmac->stop();
+					hideTimer();
+			
+					scene->enter();
+					rocketMoveTimer->start();
+				}
+				timer->start();
+			}
+		}
+		key_index++;
+		return true;
+	});
+
+
+// GAME 3 제어함수
 	rocketStopTimer->setOnTimerCallback([&](TimerPtr timer)->bool {
 		explode->hide();
-		hearts[--heartCount]->setImage("images/emptyheart.png");
+		hearts[--heartCount]->setImage("images3/emptyheart.png");
 
 		if (heartCount == 0) endGame();
 
@@ -246,18 +491,13 @@ int main()
 		});
 
 
-	// 이동제어 - 키보드 입력
 
-	// 82 83 84 85
-	int pressedkey = 0;
-	int moveX, moveY = 0;
-	auto keyMoveTimer = Timer::create(0.1f);
 
 	keyMoveTimer->setOnTimerCallback([&](TimerPtr timer)->bool
 		{
 			switch (pressedkey)
 			{
-			case 82:
+			case LEFT:
 				if (moveAvailable(-1, 0))
 				{
 					moveX = -1; moveY = 0;
@@ -267,7 +507,7 @@ int main()
 				}
 				break;
 
-			case 83:
+			case RIGHT:
 				if (moveAvailable(1, 0))
 				{
 					moveX = 1; moveY = 0;
@@ -276,7 +516,7 @@ int main()
 					keyMoveTimer->start();
 				}
 				break;
-			case 84:
+			case UP:
 				if (moveAvailable(0, -1))
 				{
 					moveX = 0; moveY = -1;
@@ -285,7 +525,7 @@ int main()
 					keyMoveTimer->start();
 				}
 				break;
-			case 85:
+			case DOWN:
 				if (moveAvailable(0, 1))
 				{
 					moveX = 0; moveY = 1;
@@ -297,7 +537,7 @@ int main()
 				break;
 			}
 			return false;
-		});
+	});
 
 	scene->setOnKeyboardCallback([&](ScenePtr scene, int key, bool pressed)->bool
 		{
@@ -311,11 +551,6 @@ int main()
 			return true;
 		});
 
-
-
-	// 로켓의 움직임을 제어 
-	auto rocketMoveTimer = Timer::create(0.5f);
-	rocketMoveTimer->start();
 
 	rocketMoveTimer->setOnTimerCallback([&](TimerPtr timer)->bool
 		{
@@ -333,7 +568,7 @@ int main()
 
 
 
-	startGame(scene);
+	startGame(startscene);
 
 	return 0;
 }
